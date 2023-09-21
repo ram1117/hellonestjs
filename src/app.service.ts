2429cffb40a6af3fa35e8dbbec3345f1cf3e23f8
ram1117 <ramkumark1@outlook.com>
2023-09-21 16:20:45 +0530

    create sources endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,6 +8,9 @@ interface GreetingsType{
 @Injectable()
 export class AppService {
   getHello(): GreetingsType[] {
-    return [{id:1,message:'Hello World!'},{id:2,message:'Herzlich Wilkomment'}];
+    return [
+      { id: 1, message: 'Hello World!' },
+      { id: 2, message: 'Herzlich Wilkomment' },
+    ];
   }
 }
